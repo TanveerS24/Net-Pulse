3149ecc39bea38acb60a2ecc2992a7f3a9f2d608
TanveerS24 <stanveer1809@gmail.com>
2026-02-15 12:49:46 +0530

    test

--- server/src/api/index.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    // Access query parameters: req.query.paramName
-    const name = req.query.name || 'World';
-    res.status(200).send(`Hello ${name}! This is a GET request response.`);
-  } else {
-    res.status(405).send('Method Not Allowed');
-  }
-}
